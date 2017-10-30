b7c35f0ba7ba2b1a8a5c26e9d5c7ff9df12711af
LaelRodrigues <laelrodrigues7@gmail.com>
2017-10-29 22:38:45 -0300

    Adicionando o arquivo main.cpp da questão 2

---
+++ src/questao2/main.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file	main3.cpp
+ * @brief	Codigo fonte de teste da funcao que calcula o
+ *			valor de uma determinada expressao escrita na 
+ *			notacao polonesa inversa
+ * @author	LaelRodrigues (laelRodrigues7@gmail.com)
+ * @since	29/10/2017
+ * @date	29/10/2017
+ */
+
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+#include <string>
+using std::string;
+using std::getline;
+
+#include "rpn.h"
+
+/** @brief Funcao principal */
+int main() {
+
+	string expressao;
+	cout << "Digite a expressao: ";
+	getline(cin,expressao);
+
+	cout << expressao.size() << endl;
+	cout << expressao << endl;
+
+	int result = calcValorExpressao(expressao);
+	cout << "O valor resultante da expressao e: " << result << endl;
+	return EXIT_SUCCESS;
+
+
+	return 0;
+}
